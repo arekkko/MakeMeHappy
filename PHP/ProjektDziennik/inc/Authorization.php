d4b31c8dbeae3c3c0fbf4a56be5e4cf276d6f3df
arekk <arekk@best.net.pl>
2019-03-04 08:51:26 +0100

    Dodanie procesu logowania

--- PHP/ProjektDziennik/inc/Authorization.php
+++ PHP/ProjektDziennik/inc/Authorization.php
@@ -1,11 +1,11 @@
 <?php 
-
-include 'config.php'; 
         
 class Authorization {
     protected $con; 
     
     public function __construct(){
+        
+        include 'config.php'; 
         
         //Create connect 
         $this->con = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE); 
@@ -17,22 +17,63 @@ class Authorization {
         
     }
     
-    //Login process
-    public function do_login($login, $password){
+    //Registration process
+    public function register_user($login, $password){
         
         //$password = md5($password); 
-        $sql = "SELECT * FROM nauczyciele WHERE login='${login}' AND password='${password}'"; 
+        $sql = "SELECT id_osoby FROM nauczyciele WHERE login='${login}' UNION SELECT id_osoby FROM uczniowie WHERE login='${login}''"; 
         
-        //check if user is in table nauczyciele
+        //check if user is in database
         $check = $this->con->query($sql); 
         
         if($check->num_rows){
             
+        }else{
+            $err = 'Dany użytkownik już istnieje'; 
+            $this->error($err);
         }
         
     }
     
-    public function error($error){
-        echo "<p class=\"error\">Wystąpił błąd: ${$error}. Skontaktuj się z administratorem.</p>"; 
+    //Login Process
+    public function login_process($login, $password){
+        
+        //$password = md5($password); 
+        
+        $sql = "SELECT id_osoby FROM nauczyciele WHERE login='${login}' AND password='${password}' UNION SELECT id_osoby FROM uczniowie WHERE login='${login}' AND password='${password}'"; 
+        
+        //check if user is in database
+        $query = mysqli_query($this->con, $sql);
+        
+        //get data of query
+        $result = mysqli_fetch_assoc($query); 
+        
+        if($query->num_rows){
+            
+            //Login process - success
+            echo 'Dobry uzytkownik';
+            
+            $_SESSION['login']   = true; 
+            $_SESSION['id_osoby'] = $result['id_osoby'];
+            
+            return true;
+            
+        }else{
+            $err = 'Zły login lub hasło. Sprobuj ponownie.'; 
+            $this->display_error($err);
+            return false; 
+        }
+    }
+    
+    //Logout process
+    public function logout_process(){
+        $_SESSION['login'] = false; 
+        
+        session_destroy(); 
+    }
+    
+    public function display_error($error){
+        die("<p class=\"error\">Wystąpił błąd: <strong>${error}</strong>.</p>"); 
+        return 0; 
     }
 }
--- PHP/ProjektDziennik/index.php
+++ PHP/ProjektDziennik/index.php
@@ -1,16 +1,26 @@
 <html>
 <head>
+
+</head>
 <body>
-<!-- http://www.w3programmers.com/login-registration-using-oop/ -->     
+<!-- http://www.w3programmers.com/login-registration-using-oop/ -->   
+    
+<!-- w tabeli uczniowie i tabeli nauczyciele trzeba zmienić nazwę komuny z id na id_osoby (tak aby tu i tu byla jednakowa) --> 
     
 <?php 
 include 'inc/Authorization.php'; 
     
 $logowanie = new Authorization(); 
     
-$logowanie->do_login('user', 'user');
+$logowanie->login_process('user', 'user');
     
+if($_SESSION){
+    echo 'Witaj, zalogowałeś się poprawnie'; 
+    
+    echo '<a href="?logout=true">Wyloguj się</a>'; 
+    
+}
 ?>
+
 </body>    
-</head>
 </html>
