4c4f4343727df92d0c271dca5b27faeeab1dee31
arekk <arekk@best.net.pl>
2019-03-30 13:24:46 +0100

    Dodanie fragmentu lekcji JS

--- JS/kursNauka/sekcja3/js/scripts.js
+++ JS/kursNauka/sekcja3/js/scripts.js
@@ -1,14 +1,27 @@
 (function(){
 
-  var title = document.querySelectorAll("[title]");
-
-  var container = document.querySelectorAll(".container");
-
-  function handleClick(){
-
+  function showTooltip(e){
+    console.log(e.target);
   }
 
-  container.addEventListener("click", handleClick); 
+  function hideTooltip(){
+    console.log("Chowam tooltip");
+  }
 
+  var title = document.querySelectorAll("[title]");
+
+  var container = document.querySelector(".container");
+
+  for( var i = 0; i < title.length; i++){
+    title[i].addEventListener("mouseenter", showTooltip, false);
+    title[i].addEventListener("mouseleave", hideTooltip, false);
+  }
+/*
+  container.addEventListener("click", function(){
+
+    console.log("Klik");
+
+  }, false);
+*/
 
 })();
