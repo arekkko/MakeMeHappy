26bd7a388e2914a5697a29d7e01d08578615bc96
arekk <arekk@best.net.pl>
2019-03-30 13:08:04 +0100

    Dodanie nowego folderu z nauka JS

---
+++ JS/kursNauka/sekcja3/js/scripts.js
@@ -0,0 +1,14 @@
+(function(){
+
+  var title = document.querySelectorAll("[title]");
+
+  var container = document.querySelectorAll(".container");
+
+  function handleClick(){
+
+  }
+
+  container.addEventListener("click", handleClick); 
+
+
+})();
